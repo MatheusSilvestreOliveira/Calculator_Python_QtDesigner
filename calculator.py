a29a3c260554bf5ff0ab0ee484a7e44044eb0337
Matheus Silvestre <mathsylvester13@gmail.com>
2022-08-12 16:12:57 -0300

    Improving code organization

--- calculator.py
+++ calculator.py
@@ -6,16 +6,42 @@ import sys
 class Calculator:
     def __init__(self, design_file):
         self.design = design_file
-        self.design.setFixedSize(400,200)
-        self.design.inputWindow.setDisabled(True)
-        self.design.inputWindow.setSizePolicy(QSizePolicy.Preferred, QSizePolicy.Expanding)
-        self.design.inputWindow.setStyleSheet('* {background: #FFF; color: #000; font-size: 30px;}')
+        self.window_settings()
         self.btn_manager()
 
     def show(self):
         self.design.show()
 
     def btn_manager(self):
+        self.set_btn_design()
+        self.set_number_btn_function()
+        self.set_operation_btn_function()
+
+        self.design.btnClear.clicked.connect(
+            lambda: self.design.inputWindow.setText('')
+        )
+        self.design.btnErase.clicked.connect(
+            lambda: self.design.inputWindow.setText(self.design.inputWindow.text()[:-1])
+        )
+        self.design.btnEqual.clicked.connect(self.equal_btn)
+
+    def window_settings(self):
+        self.design.setFixedSize(400, 200)
+        self.design.inputWindow.setDisabled(True)
+        self.design.inputWindow.setSizePolicy(QSizePolicy.Preferred, QSizePolicy.Expanding)
+        self.design.inputWindow.setStyleSheet('* {background: #FFF; color: #000; font-size: 30px;}')
+
+    def set_btn_design(self):
+        self.design.btnSum.setStyleSheet('* {background: #55abcc; color: #000;}')
+        self.design.btnSub.setStyleSheet('* {background: #55abcc; color: #000;}')
+        self.design.btnMult.setStyleSheet('* {background: #55abcc; color: #000;}')
+        self.design.btnDiv.setStyleSheet('* {background: #55abcc; color: #000;}')
+        self.design.btnClear.setStyleSheet('* {background: #c87209; color: #000; font-weight: 900}')
+        self.design.btnErase.setStyleSheet('* {background: #c87209; color: #000; font-weight: 900}')
+        self.design.btnEqual.setSizePolicy(QSizePolicy.Preferred, QSizePolicy.Expanding)
+        self.design.btnEqual.setStyleSheet('* {background: #65d55d; color: #000; font-weight: 900}')
+
+    def set_number_btn_function(self):
         self.design.btn0.clicked.connect(
             lambda: self.increment_display('0')
         )
@@ -50,39 +76,19 @@ class Calculator:
             lambda: self.increment_display('.')
         )
 
-        self.design.btnSum.setStyleSheet('* {background: #55abcc; color: #000;}')
+    def set_operation_btn_function(self):
         self.design.btnSum.clicked.connect(
             lambda: self.increment_display('+')
         )
-
-        self.design.btnSub.setStyleSheet('* {background: #55abcc; color: #000;}')
         self.design.btnSub.clicked.connect(
             lambda: self.increment_display('-')
         )
-
-        self.design.btnMult.setStyleSheet('* {background: #55abcc; color: #000;}')
         self.design.btnMult.clicked.connect(
             lambda: self.increment_display('*')
         )
-
-        self.design.btnDiv.setStyleSheet('* {background: #55abcc; color: #000;}')
         self.design.btnDiv.clicked.connect(
             lambda: self.increment_display('/')
         )
-
-        self.design.btnClear.clicked.connect(
-            lambda: self.design.inputWindow.setText('')
-        )
-        self.design.btnClear.setStyleSheet('* {background: #c87209; color: #000; font-weight: 900}')
-
-        self.design.btnErase.clicked.connect(
-            lambda: self.design.inputWindow.setText(self.design.inputWindow.text()[:-1])
-        )
-        self.design.btnErase.setStyleSheet('* {background: #c87209; color: #000; font-weight: 900}')
-
-        self.design.btnEqual.clicked.connect(self.equal_btn)
-        self.design.btnEqual.setSizePolicy(QSizePolicy.Preferred, QSizePolicy.Expanding)
-        self.design.btnEqual.setStyleSheet('* {background: #65d55d; color: #000; font-weight: 900}')
 
     def increment_display(self, character):
         self.design.inputWindow.setText(self.design.inputWindow.text() + character)
